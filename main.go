fca524e4c1d5c238b9253f9d9f88e4cf4346947b
Jawed khelil <jkhelil@redhat.com>
2024-02-19 12:25:36 +0100

    buikd

--- main.go
+++ main.go
@@ -1,22 +1,80 @@
 package main
 
 import (
-	"log"
-	"net/http"
+	"fmt"
+	"os"
+	"os/user"
+	"path/filepath"
+	"time"
 
-	filters "gohandler/pkg/filters"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
+	v1 "k8s.io/client-go/1.5/pkg/api/v1"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
-	mux := http.NewServeMux()
-	filters.AddFilterHandlers(mux)
-	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("pong"))
-	})
+	//ctx, _ := context.WithCancel(context.Background())
+	var kubeclient kubernetes.Interface
+	if kubeclient, err := loadKubernetesClientSet(); err != nil {
+		fmt.Printf("Failed to load kubernetes API", err.Error())
+		os.Exit(1)
+	} else {
+		kubeclient = kubeclient
+	}
+	shareInformerFactory := informers.NewSharedInformerFactoryWithOptions(kubeclient, time.Minute*5, informers.WithTweakListOptions(func(options *v1.ListOptions) {
+		options.FieldSelector = fmt.Sprintf("spec.nodeName=%s", "kind-control-plane")
+	}))
+	podInformer := shareInformerFactory.Core().V1().Pods().Informer()
+	podInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			pod := obj.(*corev1.Pod)
+			fmt.Printf("Pod Added: %s\n", pod.Name)
 
-	mux.HandleFunc("/ok", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("ok\n"))
-	})
-	log.Fatal(http.ListenAndServe("127.1:8080", mux))
+		},
+		UpdateFunc: func(oldObj, newObj interface{}) {
+			newPod := newObj.(*corev1.Pod)
+			fmt.Printf("Pod Updated: %s\n", newPod.Name)
+		},
 
+		DeleteFunc: func(obj interface{}) {
+			pod := obj.(*corev1.Pod)
+			fmt.Printf("Pod Deleted: %s\n", pod.Name)
+		},
+	})
+	shareInformerFactory.Start(wait.NeverStop)
+	for gvr, ok := range shareInformerFactory.WaitForCacheSync(wait.NeverStop) {
+		if !ok {
+			fmt.Errorf(fmt.Sprintf("Failed to sync cache for resource %v", gvr))
+		}
+	}
+	fmt.Println("start kubelet informer...")
+}
+
+func GetConfig() (*rest.Config, error) {
+	if len(os.Getenv("KUBECONFIG")) > 0 {
+		return clientcmd.BuildConfigFromFlags("", os.Getenv("KUBECONFIG"))
+	}
+	if c, err := rest.InClusterConfig(); err == nil {
+		return c, nil
+	}
+	if usr, err := user.Current(); err == nil {
+		if c, err := clientcmd.BuildConfigFromFlags("", filepath.Join(usr.HomeDir, ".kube", "config")); err == nil {
+			return c, nil
+		}
+	}
+	return nil, fmt.Errorf("could not locate kubeconfig")
+}
+
+func loadKubernetesClientSet() (kubernetes.Interface, error) {
+	kubeRestConfig, err := GetConfig()
+	//fmt.Println(kubeRestConfig)
+	if err != nil {
+		return nil, err
+	}
+	return kubernetes.NewForConfig(kubeRestConfig)
 }
---
+++ serve.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	filters "gohandler/pkg/filters"
+)
+
+func serve() {
+	mux := http.NewServeMux()
+	filters.AddFilterHandlers(mux)
+	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("pong"))
+	})
+
+	mux.HandleFunc("/ok", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("ok\n"))
+	})
+	log.Fatal(http.ListenAndServe("127.1:8080", mux))
+
+}
